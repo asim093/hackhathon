bddf32a8c0a88101d03f4aad40771e88a391f0f7
asim093 <asimusman8899@gmail.com>
2025-01-06 08:02:50 -0800

    hackhathon task complete with all functionality

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ import swaggerSpec from "./swagger.js";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.use(
   cors({
